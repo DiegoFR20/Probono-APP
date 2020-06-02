ad399a058b22c34312062264329fd2a0e6fa714a
DiegoFR <diegofreire8482@hotmail.com>
2020-06-02 11:55:28 -0300

    Corrigido algumas rotas e criadas outras, validação de formulário quase funcionando, falta aperecer a mensagem

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,13 @@
 var mongodb = require('mongodb').MongoClient,
+    objectID = require('mongodb').ObjectID,
     assert = require('assert');
 
 var dbName = 'probono';
 var mongoURL = 'mongodb://localhost:27017';
 
-var connMongoDB = function () {
+var connMongoDB = function (data) {
     mongodb.connect(mongoURL, function (err, client) {
         assert.equal(null, err);
-        console.log("Contectado com sucesso");
         const db = client.db(dbName);
         query(db, data);
         client.close();
@@ -17,22 +17,22 @@ var connMongoDB = function () {
 function query(db, data) {
     var collection = db.collection(data.collection);
     switch (data.operacao) {
-        case 'atualizar':
-            collection.update(data.where, data.set);
-            break;
         case 'inserir':
             collection.insertOne(data.dados, data.callback);
+            break;
+        case 'atualizar':
+            collection.update(data.where, data.set);
             break;
         case 'pesquisar':
             collection.find(data.dados).toArray(data.callback);
             break;
         case 'remover':
-            data.where._id = objectId(data.where._id);
+            data.where._id = objectID(data.where._id);
             collection.remove(data.where, data.callback);
             break;
     }
 }
 
-module.exports = function(){
+module.exports = function () {
     return connMongoDB;
 }
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var expressSession = require('express-session');
-var consign = require('consign');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    expressSession = require('express-session'),
+    consign = require('consign');
 
 var app = express();
 
@@ -21,18 +21,19 @@ app.use(expressSession({
 
 consign()
     .include('app/routes')
+    .then('config/dbConnection.js')
     .then('app/models')
     .then('app/controllers')
     .into(app);
 
-app.use(function (err, req, res, next) {
-    res.status(404).render('errors/404')
-    next();
-})
+// app.use(function (err, req, res, next) {
+//     res.status(404).render('errors/404')
+//     next();
+// })
 
-app.use(function (req, res, next) {
-    res.status(500).render('errors/500')
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.status(500).render('errors/500')
+//     next();
+// });
 
 module.exports = app;
